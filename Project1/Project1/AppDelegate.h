39385cc39c283bdb3cd013a45ff5f42e6e460037
Shawn Morgart <halcyon@fullsail.edu>
2012-06-26 22:35:51 -0400

    Set up for project 1

---
+++ Project1/Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Shawn Morgart on 6/26/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project1/Project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project1
+//
+//  Created by Shawn Morgart on 6/26/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
